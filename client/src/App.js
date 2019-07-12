441d98f0047fab059523ab147623801473110d37
lamdang98 <ltdang16@earlham.edu>
2019-07-12 12:25:46 -0500

    first commit front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,84 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider, MessageList , MessageGroup, Message, MessageText , FixedWrapper , TextComposer, Row, IconButton, EmojiIcon, SendButton, AddIcon, TextInput } from '@livechat/ui-kit'
 
+const theme = {
+  vars: {
+      'primary-color': '#427fe1',
+      'secondary-color': '#fbfbfb',
+      'tertiary-color': '#fff',
+      'avatar-border-color': 'blue',
+  },
+  AgentBar: {
+      Avatar: {
+          size: '42px',
+      },
+      css: {
+          backgroundColor: 'var(--secondary-color)',
+          borderColor: 'var(--avatar-border-color)',
+      }
+  },
+  Message: {
+      css: {
+          fontWeight: 'bold',
+          backgroundColor: 'lightblue',
+          borderRadius: '10px'
+
+      },
+  
+  },
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div style={{ maxWidth: '80%', height: '100%' }}>
+      <MessageList active>
+        <MessageGroup isOwn={true}>
+        <Message date="21:38" isOwn={true} authorName="Visitor" radiusType='single'>
+        <MessageText>
+          I love them
+          soooooooooooo
+          much!
+        </MessageText>
+      </Message>
+      </MessageGroup>
+
+
+        <MessageGroup>
+        <Message date="21:38"  authorName="Visitor" radiusType='single'>
+        <MessageText>
+          I love them
+          soooooooooooo
+          much!
+        </MessageText>
+      </Message>
+      </MessageGroup>
+      </MessageList>
+      <TextComposer >
+        <Row align="center">
+          <IconButton fit>
+            <AddIcon />
+          </IconButton>
+          <TextInput fill />
+          <SendButton fit />
+        </Row>
+
+        <Row verticalAlign="center" justify="right">
+          <IconButton fit>
+            <EmojiIcon />
+          </IconButton>
+        </Row>
+      </TextComposer>
+      </div>
+    </ThemeProvider>
+
+
+
+
+
+
+    
   );
 }
 
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+
+
+
+
+
+
+      <div className="panel-footer">Hello
+      </div>
+    </div>
+  );
+}
+
+export default App;
